6f89bac276fa3d208f6236534917d4b161a82f25
axmand <815966508@qq.com>
2017-05-12 22:10:26 +0800

    add config

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+	config.set({
+		basePath: '.',
+		frameworks: ['mocha', 'chai'],
+		files: [
+			// Watch src files for changes but
+			// don't load them into the browser.
+			{ pattern: 'src/**/*.js', included: false },
+			'test/**/*.spec.js',
+		],
+
+		preprocessors: {
+			//'test/buble/**/*.spec.js': ['rollup'],
+			'src/**/*.js': ['rollupBabel'],
+			'test/**/*.spec.js': ['rollupBabel'],
+		},
+
+		rollupPreprocessor: {
+			plugins: [
+				require('rollup-plugin-buble')(),
+			],
+			format: 'iife',
+			moduleName: 'test',//<your_project>
+			sourceMap: 'inline',
+		},
+
+		customPreprocessors: {
+			// Clones the base preprocessor, but overwrites
+			// its options with those defined below.
+			rollupBabel: {
+				base: 'rollup',
+				options: {
+					// In this case, to use
+					// a different transpiler:
+					plugins: [
+						require('rollup-plugin-babel')(),
+					],
+				}
+			}
+		}
+	});
+};
---
+++ test/add.spec.js
@@ -0,0 +1,9 @@
+describe('#1 test add.js funciont', () => {
+
+    describe("", () => {
+        it("ve2向量基本运算", () => {
+            expect("1").to.be.eqls("1");
+        });
+    });
+
+});
