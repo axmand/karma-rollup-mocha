32b18cde4344713b5e2716c7247218bea00d446a
axmand <815966508@qq.com>
2017-05-14 00:39:34 +0800

    add more spec.js

---
+++ src/sub.js
@@ -0,0 +1,5 @@
+let sub=(a,b)=>{
+    return a-b;
+};
+
+export default sub;
--- test/add.spec.js
+++ test/add.spec.js
@@ -1,13 +1,23 @@
 import add from './../src/add';
+import sub from './../src/sub';
 
 describe('#1 test add.js funciont', () => {
 
-    describe("", () => {
+    describe("1.add", () => {
         it("ve2向量基本运算", () => {
-            let a=1,b=2;
-            let c=add(a,b);
+            let a = 1, b = 2;
+            let c = add(a, b);
             expect(c).to.be.eqls(3);
         });
     });
+
+    describe("2.sub", () => {
+        it("ve2向量基本运算", () => {
+            let a = 1, b = 2;
+            let c = sub(a, b);
+            expect(c).to.be.eqls(-1);
+        });
+    });
+
 
 });
